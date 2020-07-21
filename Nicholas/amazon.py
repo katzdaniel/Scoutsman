97a3e481fc0f9f1e20a740d487a11f59d3935aa0
Nicholas Contreras <nicholasc2000@yahoo.com>
2020-07-21 13:44:50 -0700

    added ratings and price per unit retrevial

--- Nicholas/amazon.py
+++ Nicholas/amazon.py
@@ -28,16 +28,29 @@ def page_loaded():
         full_page = full_page[full_page.find('>') + 1:]
         cur_product_name = full_page[:full_page.find('</span>')]
 
-        if full_page.find('<span class=\"a-offscreen\">') > -1:
-            full_page = full_page[full_page.find('<span class=\"a-offscreen\">'):]
+        if full_page.find('<span class=\"a-icon-alt\">') > -1:
+            full_page = full_page[full_page.find('<span class=\"a-icon-alt\">'):]
             full_page = full_page[full_page.find('>') + 1:]
-            cur_product_price = full_page[:full_page.find('</span>')]
+            cur_product_rating = full_page[:full_page.find('</span>')]
 
-            items_found += 1
+            if full_page.find('<span class=\"a-offscreen\">') > -1:
+                full_page = full_page[full_page.find('<span class=\"a-offscreen\">'):]
+                full_page = full_page[full_page.find('>') + 1:]
+                cur_product_price = full_page[:full_page.find('</span>')]
+
+                price_per_unit_index = full_page.find('<span class=\"a-size-base a-color-secondary\">')
+                if price_per_unit_index != -1 and price_per_unit_index < full_page.find('</a>'):
+                    full_page = full_page[full_page.find('<span class=\"a-size-base a-color-secondary\">'):]
+                    full_page = full_page[full_page.find('>') + 1:]
+                    cur_product_price = full_page[:full_page.find('</span>')]
+
+                print(cur_product_name + ' = ' + cur_product_rating + ' = ' + cur_product_price)
+                items_found += 1
 
     total_items_found += items_found
     total_time_elapsed = time.time() - start_time
     items_per_second = total_items_found / total_time_elapsed
+    print('--------------------------------------------------')
     print('Found ' + str(total_items_found) + ' in ' + str(round(total_time_elapsed)) + ' seconds')
     print(str(round(items_per_second)) + ' items per second')
 
@@ -46,6 +59,8 @@ def page_loaded():
         cur_page = 1
     else:
         cur_page += 1
+    
+    print('--------------------------------------------------')
 
     webview.windows[0].load_url('https://www.amazon.com/s?k=cheese&page=' + str(cur_page))
 
